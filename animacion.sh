a889024b139a9e7c68a5048d1e78b93f4394aa9c
berryari <136492432+adianitaa@users.noreply.github.com>
2025-05-21 23:26:59 +0200

    animacion.sh

--- animacion.sh
+++ animacion.sh
@@ -34,3 +34,31 @@ for ((i=0; i<20; i++)); do
   sleep $delay
 done
 
+# Fase 2: Aparece un dino sospechoso
+for ((i=20; i<25; i++)); do
+  clear
+  draw_stickman $i
+  draw_dino 30
+  sleep $delay
+done
+
+# Fase 3: El dino lo empieza a perseguir
+for ((i=25; i<width; i++)); do
+  clear
+  draw_stickman $i
+  draw_dino $((i - 5))
+  sleep $delay
+done
+
+# Fase final: Stickman grita y desaparece
+for ((j=0; j<3; j++)); do
+  clear
+  printf "%*s\n" 35 " o  AHHHH!"
+  printf "%*s\n" 35 "/|\\"
+  printf "%*s\n" 35 "/ \\"
+  sleep 0.3
+done
+
+clear
+echo -e "\n\n      💀 El Stickman murió. Fin."
+sleep 2
